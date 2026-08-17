54fcef33ca43772d4f47fbfe8c05b5ae39956c1b
Stuart <slykstuart@gmail.com>
2026-08-17 23:58:47 +0300

    database changed from neon to render

--- mobile/lib/core/network/api_client.dart
+++ mobile/lib/core/network/api_client.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiClient {
   final Dio dio;
   final _storage = const FlutterSecureStorage();
 
-  ApiClient() : dio = Dio(BaseOptions(baseUrl: "http://192.168.1.113:8000/api")) {
+  ApiClient() : dio = Dio(BaseOptions(baseUrl: dotenv.env['API_BASE_URL'] ?? "http://192.168.1.113:8000/api")) {
     dio.interceptors.add(InterceptorsWrapper(
       onRequest: (options, handler) async {
         final token = await _storage.read(key: "access_token");
